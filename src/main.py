3d902186e0d4a20be45f595134cd3c4abe290eb1
Joshua Prettyman <j.prettyman@icloud.com>
2025-06-10 17:52:39 +0200

    Bug fixing for plots filepaths

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import argparse
 from create_viz import create_trade_visualization
 
 if __name__ == "__main__":
-    create_trade_visualization("wine", 2023)
+    parser = argparse.ArgumentParser(description="Create trade visualization")
+    parser.add_argument("commodity", help="Commodity to visualize (e.g., wine) or hscode (e.g. 2204)")
+    parser.add_argument("year", type=int, help="Year to visualize")
+    
+    args = parser.parse_args()
+    create_trade_visualization(args.commodity, args.year)
